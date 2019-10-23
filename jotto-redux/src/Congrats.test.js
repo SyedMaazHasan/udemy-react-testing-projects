cbb4529c027e0f1bd36a9d045946ccd1a9314022
Syed Maaz Hasan <49359456+SyedMaazHasan@users.noreply.github.com>
2019-10-23 12:35:48 +0530

    Update Congrats.test.js

--- jotto-redux/src/Congrats.test.js
+++ jotto-redux/src/Congrats.test.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
-import { findByTestAttr, checkProps } from '../test/testUtils';
+import checkPropTypes from 'check-prop-types';
 import Congrats from './Congrats';
 
 const defaultProps = { success: false };
 
-/**
-* Factory function to create a ShallowWrapper for the Congrats component.
-* @function setup
-* @param {object} props - Component props specific to this setup.
-* @returns {ShallowWrapper}
-*/
 const setup = (props={}) => {
   const setupProps = { ...defaultProps, ...props };
   return shallow(<Congrats {...setupProps} />)
@@ -19,20 +12,28 @@ const setup = (props={}) => {
 
 test('renders without error', () => {
   const wrapper = setup();
-  const component = findByTestAttr(wrapper, 'component-congrats');
+  const component = wrapper.find(data-test = "component-congrats");
   expect(component.length).toBe(1);
 });
+  
 test('renders no text when `success` prop is false', () => {
   const wrapper = setup({ success: false });
-  const component = findByTestAttr(wrapper, 'component-congrats');
+  const component = wrapper.find(data-test = "component-congrats");
   expect(component.text()).toBe('');
 });
+  
 test('renders non-empty congrats message when `success` prop is true', () => {
   const wrapper = setup({ success: true });
-  const message = findByTestAttr(wrapper, 'congrats-message');
+  const message = wrapper.find(data-test = "congrats-message");
   expect(message.text().length).not.toBe(0);
 });
+  
 test('does not throw warning with expected props', () => {
   const expectedProps = { success: false };
-  checkProps(Congrats, expectedProps);
+    const propError = checkPropTypes(
+    Congrats.propTypes,
+    expectedProps,
+    'prop',
+    Congrats.success);
+  expect(propError).toBeUndefined();
 });
