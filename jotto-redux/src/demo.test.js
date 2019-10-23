93fdd3f294086c5a5ed603dc51c163ebcfa6439a
Syed Maaz Hasan <49359456+SyedMaazHasan@users.noreply.github.com>
2019-10-23 13:08:16 +0530

    Update demo.test.js

--- jotto-redux/src/demo.test.js
+++ jotto-redux/src/demo.test.js
@@ -1,15 +1,22 @@
-const defaultProps = { success: false };
+//syntax: checkPropTypes(componentpropTypes ka set,prop which needs tobe checked,'prop', specific prop name which needs to be checked)
 
-const setup = (props = {}) => {
-	const setupProps = { ...defaultProps, ...props };
+///HelloComponent.js///
 
-	return shallow(<Congrats {...setupProps} />);
+import PropTypes from 'prop-types';
+
+const HelloComponent = ({ name }) => <h1>Hi, {name}</h1>;
+
+HelloComponent.propTypes = {
+	name: PropTypes.string.isRequired
 };
 
-test('test with default props', () => {
-	const wrapper = setup();
-});
+///HelloComponent.test.js/////
 
-test('test when user provides props', () => {
-	const wrapper = setup({ success: true });
+import checkPropTypes from 'check-prop-types';
+
+test('does not throw warning with expected props', () => {
+	
+	let result = checkPropTypes(HelloComponent.propTypes, { name: 'Julia' }, 'prop', HelloComponent.name);
+	
+	expect(result).toBeUndefined();
 });
